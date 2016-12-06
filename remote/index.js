787b2bded4d5f207bf9e420d12bac27f7fb84bbb
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-12-06 16:39:30 +0530

    Promisify everything!

--- remote/index.js
+++ remote/index.js
@@ -1,20 +1,33 @@
-const page = require('webpage').create();
+const phantom = require('phantom');
+
+/* Pre recorded vhs.events */
 const testEvents = JSON.stringify(require('./test-events.json'));
 const url = 'http://localhost:3000';
 
-page.open(url, function(status) {
-    /* Time out for page load complete */
-    window.setTimeout(function () {
-        /*
-        * Execute functions on the page using evaluate
-        * Passing testEvents as a variable
-        */
-        page.evaluate(function(testEvents) {
-            localStorage.setItem('vhs', testEvents);
-            window.vhs.setupPlayback();
-        }, testEvents);
-
-        /* All done, bye */
-        phantom.exit();
-    }, 1000);
+/* Create phantom instance */
+phantom.create()
+/* New page instance */
+.then(instance => instance.createPage())
+/* All console messages should come to terminal */
+.then(page => {
+    page.property('onConsoleMessage', (msg) => {
+        console.log(msg);
+    });
+    page.property('onError', (msg) => {
+        console.log(msg);
+    });
+    return page;
+})
+.then(page => page.open(url).then(() => page))
+.then(page => {
+    /* Can execute functions in page.evaluate */
+    return page.evaluate(function(testEvents) {
+        localStorage.setItem('vhs', testEvents);
+        window.vhs.setupPlayback();
+        return 'done';
+    }, testEvents);
+})
+.then((res) => {
+    console.log('result', res);
+    return page;
 });
--- remote/lib.js
+++ remote/lib.js
@@ -1,25 +1,38 @@
 'use strict';
 
-var page = require('webpage').create();
+var phantom = require('phantom');
+
+/* Pre recorded vhs.events */
 var testEvents = JSON.stringify(require('./test-events.json'));
 var url = 'http://localhost:3000';
 
-page.open(url, function (status) {
-    /* Time out for page load complete */
-    window.setTimeout(function () {
-        /*
-        * Execute functions on the page using evaluate
-        * Passing testEvents as a variable
-        */
-        var complete = page.evaluate(function (testEvents) {
-            localStorage.setItem('vhs', testEvents);
-            console.log(window.vhs.setupPlayback);
-            //window.vhs.setupPlayback();
-        }, testEvents);
-
-        console.log(complete);
-
-        /* All done, bye */
-        phantom.exit();
-    }, 1000);
+/* Create phantom instance */
+phantom.create()
+/* New page instance */
+.then(function (instance) {
+    return instance.createPage();
+})
+/* All console messages should come to terminal */
+.then(function (page) {
+    page.property('onConsoleMessage', function (msg) {
+        console.log(msg);
+    });
+    page.property('onError', function (msg) {
+        console.log(msg);
+    });
+    return page;
+}).then(function (page) {
+    return page.open(url).then(function () {
+        return page;
+    });
+}).then(function (page) {
+    /* Can execute functions in page.evaluate */
+    return page.evaluate(function (testEvents) {
+        localStorage.setItem('vhs', testEvents);
+        window.vhs.setupPlayback();
+        return 'done';
+    }, testEvents);
+}).then(function (res) {
+    console.log('result', res);
+    return page;
 });
