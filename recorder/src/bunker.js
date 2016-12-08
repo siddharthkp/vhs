97f1ab57f5325314ecf2e6088d720c875aeeae1d
chandureddivari <chandu.reddivari1504@gmail.com>
2016-12-08 17:04:14 +0530

    passing name in the args

--- recorder/src/bunker.js
+++ recorder/src/bunker.js
@@ -4,9 +4,9 @@ firebase.initializeApp(appConfig);
 const dbLocation = appConfig.databaseURL;
 const newTapeRef = firebase.database().ref('tapes');
 
-const storeEvents = (events) => {
+const storeEvents = (name, events) => {
     newTapeRef.push({
-        'name': 'demo',
+        'name': name || 'Anonymous', //TODO: Change the name to name of the test
         'events': events
     }).then(function (res) {
         // Call back after pushing events
