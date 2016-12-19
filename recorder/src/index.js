48a0e6649beb9b962deb18e36a294674e62f455a
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-12-19 17:28:03 +0530

    Bringing back key combo gap

--- recorder/src/index.js
+++ recorder/src/index.js
@@ -189,10 +189,25 @@ const keypress = ({selector, which}, resolve) => {
 };
 
 const keyCombo = ({selector, whichs}, resolve) => {
-    for (let i = 0; i < whichs.length; i++) {
-        keypress({selector, which: whichs[i]});
-    }
-    resolve();
+    /*
+        Playing key events with a tiny gap
+        for a smooth animation with the sidebar
+        In remote mode, we can't see the UI,
+        optimising for faster build times there.
+    */
+    let keyComboGap = 100;
+        if (vhs.remote) keyComboGap = 0;
+
+        let i = 0;
+        let interval = window.setInterval(() => {
+        if (!whichs[i]) {
+            window.clearInterval(interval);
+            resolve();
+        } else {
+            keypress({selector, which: whichs[i]});
+            i++;
+        }
+    }, keyComboGap);
 };
 
 const wait = ({duration}, resolve) => {
