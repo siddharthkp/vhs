69b5ea9e4f76e54075d9620b88d09fc78dd68757
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-12-20 15:07:47 +0530

    Chrome extension 0.0.1

---
+++ recorder/crx/background.js
@@ -0,0 +1,7 @@
+// this is the background code...
+
+// listen for our browerAction to be clicked
+chrome.browserAction.onClicked.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+    chrome.tabs.executeScript(null, { file: "injecter.js" });
+});
