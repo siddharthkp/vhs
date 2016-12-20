9564fb6822e163ee2fe1102f325cf3b90722c060
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-12-20 18:55:22 +0530

    Make remote tests work with jquery

--- remote/test-server.js
+++ remote/test-server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const nodeStatic = require('node-static');
 const port = 3000;
 
-const demo = new nodeStatic.Server('../demo');
+const demo = new nodeStatic.Server('../demos/jquery');
 const server = http.createServer((request, response) => {
     request.addListener('end', () => demo.serve(request, response)).resume();
 }).listen(port, () => console.log(`Running server on port ${port}`));
